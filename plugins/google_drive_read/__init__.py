9643b7657296763bdcb9144acee4bd7acc118669
TGC <truegoodcraft@gmail.com>
2025-11-28 23:22:55 -0500

    Delete plugins/google_drive_read directory

--- plugins/google_drive_read/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Google Drive read-only plugin for TGC Core v2."""
-
-PLUGIN_ID = "google_drive_read"
-VERSION = "0.1.0"
-PROVIDES = ["google.drive.read"]
-REQUIRES = []
-STAGES = ["service"]
-TRUST_TIER = "untrusted"
-
